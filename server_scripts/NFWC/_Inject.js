6917671a857a6ff51f30a6af4cf4390d4ba4ba59
YukkuriC <799433638@qq.com>
2024-08-10 02:31:47 +0800

    器官功能装饰器测试

---
+++ server_scripts/NFWC/_Inject.js
@@ -0,0 +1,20 @@
+ServerEvents.lowPriorityData(e => {
+    InjectFunc(organPlayerDamageOnlyStrategies, 'kubejs:the_third_eye', INJECTORS.DAMAGE.THIRD_EYE)
+})
+
+function InjectFunc(container, key, append) {
+    container[key] = (f => (arg0, arg1, arg2) => {
+        f(arg0, arg1, arg2)
+        append(arg0, arg1, arg2)
+    })(container[key])
+}
+
+const INJECTORS = {
+    DAMAGE: {
+        THIRD_EYE: (event, organ, data) => {
+            organPlayerDamageOnlyStrategies['kubejs:lost_paradise'](event, organ, data)
+            let player = event.source.player
+            updateWarpCount(player, player.persistentData.getInt(warpCount) + 5)
+        },
+    },
+}
