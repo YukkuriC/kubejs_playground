c2fea8354b67f2314b2702de8e807a1a3d151fee
YukkuriC <799433638@qq.com>
2024-08-11 21:36:20 +0800

    糖果能吃

---
+++ startup_scripts/NFWC/item_modify.js
@@ -0,0 +1,14 @@
+ItemEvents.modification(e => {
+    // 糖果当然能吃辣
+    for (let id of ['kubejs:candy_heart', 'kubejs:candy_stomach', 'kubejs:candy_pancreas']) {
+        e.modify(id, item => {
+            item.foodProperties = food => {
+                food.hunger(16)
+                food.saturation(2.5)
+                food.effect('minecraft:regeneration', 20 * 45, 0, 1)
+                food.effect('minecraft:resistance', 20 * 45, 0, 1)
+            }
+            item.getItemBuilder().tag('kubejs:food')
+        })
+    }
+})
