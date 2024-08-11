3a7dd802246b6fbde1c87976d08774d803430611
YukkuriC <799433638@qq.com>
2024-08-11 14:50:41 +0800

    减少附魔刷刷

--- server_scripts/NFWC/_Inject.js
+++ server_scripts/NFWC/_Inject.js
@@ -1,5 +1,7 @@
 ServerEvents.lowPriorityData(e => {
     InjectFunc(organPlayerDamageOnlyStrategies, 'kubejs:the_third_eye', INJECTORS.DAMAGE.THIRD_EYE)
+    organPlayerEnchantOnlyStrategies['kubejs:pandora_active'] = organPlayerEnchantOnlyStrategies['kubejs:pandora_inactive'] =
+        INJECTORS.ENCHANT.PANDORA
 })
 
 function InjectFunc(container, key, append) {
@@ -15,6 +17,32 @@ const INJECTORS = {
             // organPlayerDamageOnlyStrategies['kubejs:lost_paradise'](event, organ, data)
             let player = event.source.player
             updateWarpCount(player, player.persistentData.getInt(warpCount) + 5)
+        },
+    },
+    ENCHANT: {
+        PANDORA: function (event, organ) {
+            for (let slot = 0; slot < 3; slot++) {
+                let enchantSlot = event.get(slot)
+                let allLevel = 0
+                enchantSlot.forEachEnchantments((enchantment, level) => {
+                    if (level >= 5) level = 7
+                    else if (level >= 3) level = 5
+                    allLevel = allLevel + level
+                })
+                let needEnchantList = []
+                enchantSlot.forEachEnchantments((enchantment, level) => {
+                    needEnchantList.push(enchantment)
+                })
+                enchantSlot.clearEnchantments()
+                needEnchantList.forEach(needEnchant => {
+                    enchantSlot.addEnchantment(needEnchant, allLevel)
+                })
+                enchantSlot.updateClue()
+            }
+            if (Math.random() < 0.2) {
+                let count = event.player.persistentData.getInt(warpCount) ?? 0
+                updateWarpCount(event.player, count + 1)
+            }
         },
     },
 }
