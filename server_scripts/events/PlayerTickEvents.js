048cae0282059117ddd4b9ec1dd45af3c3d90174
YukkuriC <799433638@qq.com>
2024-08-13 10:31:56 +0800

    bugfix: 运行初期以外无法绑定PlayerEvents.tick

--- server_scripts/events/PlayerTickEvents.js
+++ server_scripts/events/PlayerTickEvents.js
@@ -1,19 +1,16 @@
 /**@type {Events.PlayerTickEvents}*/
 const PlayerTickEvents = (() => {
     /**@type {Map<number,Events.Event>}*/
-    let cache = null
+    let cache = new Map()
+    PlayerEvents.tick(e => {
+        for (let pair of cache.entries()) {
+            if (e.player.age % pair[0]) return
+            pair[1].fire1(e)
+        }
+    })
     return {
         every(n) {
             n = Math.max(1, Math.round(n))
-            if (!cache) {
-                cache = new Map()
-                PlayerEvents.tick(e => {
-                    for (let pair of cache.entries()) {
-                        if (e.player.age % pair[0]) return
-                        pair[1].fire1(e)
-                    }
-                })
-            }
             if (!cache.has(n)) {
                 cache.set(n, new Event(`every ${n} ticks`))
             }
