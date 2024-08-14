a1c0e1d63fdd314fe107b426c762226f018f0c37
YukkuriC <799433638@qq.com>
2024-08-14 23:04:43 +0800

    使用简化写法，减少原生类引用

--- startup_scripts/misc/CombinedBuffs.js
+++ startup_scripts/misc/CombinedBuffs.js
@@ -7,17 +7,11 @@ const buffsToAdd = [
 ForgeEvents.onEvent('net.minecraftforge.event.entity.living.MobEffectEvent$Added', e => {
     const self = e.getEntity()
     const effect = e.getEffectInstance()
-    if (!(self instanceof $Player)) return
+    if (!self.isPlayer()) return
     if (effect.descriptionId === 'effect.minecraft.regeneration' && effect.duration == 340 && effect.amplifier == 0) {
+        let pool = self.potionEffects
         for (const [id, lvl] of buffsToAdd) {
-            const newEffect = new $MobEffectInstance(
-                id,
-                340, // duration ticks
-                lvl - 1, // amplifier = (level - 1)
-                true, // ambient?
-                true, // show in hud?
-            )
-            self.addEffect(newEffect, self)
+            pool.add(id, 340, lvl - 1, true, true)
         }
     }
 })
