4953c074d0bc82ed2dd13b4d7b740373205a7ee0
YukkuriC <799433638@qq.com>
2024-09-01 18:27:01 +0800

    纤维属性更新至mekasuit

---
+++ server_scripts/InjectMekasuitItem.js
@@ -0,0 +1,31 @@
+// inject on fibers edited
+EntityEvents.spawned('minecraft:item', e => {
+    let item = e.entity.item
+    if (!item.id.startsWith('mekanism:mekasuit_')) return
+    global.InjectMekasuit(item)
+})
+for (const slot of ['helmet', 'bodyarmor', 'pants', 'boots'])
+    PlayerEvents.inventoryChanged(`mekanism:mekasuit_${slot}`, e => global.InjectMekasuit(e.item))
+
+global.InjectMekasuit = (stack, slot) => {
+    if (!slot) slot = Client.player.getEquipmentSlotForItem(stack)
+    let ARS = global.ARS,
+        MEK = global.MEK
+
+    for (let pool of [ARS, MEK]) if (!pool.SUIT) pool.SUIT = pool.SUIT_BASE().map(x => x.get()) // grab cache
+
+    if (stack.nbt) delete stack.nbt.AttributeModifiers
+    let mainMap = MEK.SUIT[slot.getIndex()].getAttributeModifiers(slot, stack)
+    for (let pair of mainMap.entries()) {
+        let { key, value } = pair
+        stack.addAttributeModifier(key, value, slot)
+    }
+    let attrMap = ARS.SUIT[slot.getIndex()].getAttributeModifiers(slot, stack)
+    for (let pair of attrMap.entries()) {
+        let { key, value } = pair
+        if (value.name.startsWith('Armor ')) {
+            continue
+        }
+        stack.addAttributeModifier(key, value, slot)
+    }
+}
--- startup_scripts/ArmorLinker.js
+++ startup_scripts/ArmorLinker.js
@@ -1,12 +1,18 @@
 {
     let Arrays = Java.loadClass('java.util.Arrays')
 
-    let ARS = {}
+    let ARS = {
+        SUIT_BASE: () => [
+            ARS.ItemsRegistry.BATTLEMAGE_BOOTS,
+            ARS.ItemsRegistry.BATTLEMAGE_LEGGINGS,
+            ARS.ItemsRegistry.BATTLEMAGE_ROBES,
+            ARS.ItemsRegistry.BATTLEMAGE_HOOD,
+        ],
+    }
     for (let cls of [
         'com.hollingsworth.arsnouveau.api.perk.ArmorPerkHolder',
         'com.hollingsworth.arsnouveau.api.registry.PerkRegistry',
         'com.hollingsworth.arsnouveau.setup.registry.ItemsRegistry',
-        'com.hollingsworth.arsnouveau.api.util.PerkUtil',
         'com.hollingsworth.arsnouveau.api.perk.PerkSlot',
     ]) {
         let tmp = cls.split(/\./g)
@@ -17,15 +23,17 @@
 
     let MEK = {
         MekanismItems: Java.loadClass('mekanism.common.registries.MekanismItems'),
+        SUIT_BASE: () => [
+            MEK.MekanismItems.MEKASUIT_BOOTS,
+            MEK.MekanismItems.MEKASUIT_PANTS,
+            MEK.MekanismItems.MEKASUIT_BODYARMOR,
+            MEK.MekanismItems.MEKASUIT_HELMET,
+        ],
     }
+    global.MEK = MEK
 
     StartupEvents.postInit(e => {
-        for (let item of [
-            MEK.MekanismItems.MEKASUIT_HELMET,
-            MEK.MekanismItems.MEKASUIT_BODYARMOR,
-            MEK.MekanismItems.MEKASUIT_PANTS,
-            MEK.MekanismItems.MEKASUIT_BOOTS,
-        ]) {
+        for (let item of MEK.SUIT_BASE()) {
             ARS.PerkRegistry.registerPerkProvider(
                 item,
                 stack =>
@@ -40,4 +48,10 @@
             )
         }
     })
+
+    // backup update
+    // ForgeEvents.onEvent('net.minecraftforge.event.entity.living.LivingEquipmentChangeEvent', e => {
+    //     const { slot, from, to } = e
+    //     if (slot.armor && from.id != to.id && to.id.startsWith('mekanism:mekasuit_')) global.InjectMekasuit(to, slot)
+    // })
 }
