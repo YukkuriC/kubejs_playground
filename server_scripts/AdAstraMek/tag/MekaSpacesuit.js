ecf840558581289f3ebb726c101550fe46e4a9d3
YukkuriC <799433638@qq.com>
2024-09-05 00:13:48 +0800

    补个热量免疫tag

--- server_scripts/AdAstraMek/tag/MekaSpacesuit.js
+++ server_scripts/AdAstraMek/tag/MekaSpacesuit.js
@@ -3,6 +3,7 @@ ServerEvents.tags('item', e => {
     let suits = ['helmet', 'bodyarmor', 'pants', 'boots'].map(x => `mekanism:mekasuit_${x}`)
     for (let tag of [
         'space_suit_items',
+        'heat_resistant_armor',
         // 后两个tag在1.20.1 forge发布版没生效，麻
         'space_resistant_armor',
         'oxygen_supplying_armor',
