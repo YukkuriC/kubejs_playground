1eb8bfcb2df443e4778107cc36b37390cdcde33f
YukkuriC <799433638@qq.com>
2024-10-01 01:41:34 +0800

    复制器增加神秘数字配方

--- server_scripts/TOOLS/Duper.js
+++ server_scripts/TOOLS/Duper.js
@@ -6,3 +6,16 @@ ItemEvents.firstRightClicked('yc:duper', e => {
         player.give(Item.of(i.toJson()))
     }
 })
+
+ServerEvents.recipes(e => {
+    function codeMatcher(grid, item) {
+        let total = grid.width * grid.height
+        let code = ''
+        for (let i = 0; i < total; i++) {
+            let item = grid.get(i)
+            if (item.id != 'minecraft:air') code += item.count
+        }
+        if (code == '114514' || code == '1919' || code == '810') return item
+    }
+    for (let i = 2; i <= 6; i++) e.shapeless('yc:duper', Array(i).fill('#minecraft:logs')).modifyResult(codeMatcher)
+})
