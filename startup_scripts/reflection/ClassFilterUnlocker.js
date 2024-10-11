6cfaedc6a26845fc36bcf37c8fd640c75e167b52
YukkuriC <799433638@qq.com>
2024-10-11 20:34:18 +0800

    kjs 1.19.x没有ServerScriptManager

--- startup_scripts/reflection/ClassFilterUnlocker.js
+++ startup_scripts/reflection/ClassFilterUnlocker.js
@@ -1,8 +1,8 @@
 {
     let ClassFilter = Java.loadClass('dev.latvian.mods.kubejs.util.ClassFilter')
-    global.unlockClassFilter = (javaWrapper, superCount) => {
+    global.unlockClassFilter = javaWrapper => {
         let sm = global.getField(javaWrapper, 'manager')
-        global.setField(sm, 'classFilter', ClassFilter(), 0, sm.scriptType == 'SERVER')
+        global.setField(sm, 'classFilter', ClassFilter())
     }
 
     global.unlockClassFilter(Java)
