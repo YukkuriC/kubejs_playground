ad65138718ad69bd625e3f8e823cd614bfeafcd0
YukkuriC <799433638@qq.com>
2024-10-15 00:05:26 +0800

    改从源码读取所有（静态）已注册图案

--- server_scripts/hex/Parser.js
+++ server_scripts/hex/Parser.js
@@ -1,88 +1,96 @@
-ServerEvents.commandRegistry(e => {
-    const { commands: cmd, arguments: arg } = e
-    const GetPlayer = ctx => {
-        /**@type {Player}*/
-        let player = ctx.source.entity
-        if (player && player.isPlayer()) return player
+{
+    let PatternRegistryPath = 'at.petrak.hexcasting.api.PatternRegistry'
+    let PRClass = Java.loadClass(PatternRegistryPath)
+    let PRRaw = global.loadRawClass(PatternRegistryPath)
+
+    let mapStartDir = {}
+    let mapLineDir = {}
+    'NORTH_EAST,EAST,SOUTH_EAST,SOUTH_WEST,WEST,NORTH_WEST'.split(',').forEach((x, i) => (mapStartDir[x] = `${i}b`))
+    Array.from('wedsaq').forEach((x, i) => (mapLineDir[x] = `${i}B`))
+    let seq2bytes = seq =>
+        Array.from(seq)
+            .map(x => mapLineDir[x])
+            .join(',')
+
+    let mapPatterns = {}
+    // map static class
+    let staticMap = global.getField(PRRaw, 'regularPatternLookup', 1)
+    for (let seq in staticMap) {
+        let pattern = staticMap[seq]
+        let op = pattern.opId()
+        let startDir = pattern.preferredStart()
+        let patternNBT = `{"hexcasting:data":{angles:[B;${seq2bytes(seq)}],start_dir:${
+            mapStartDir[startDir]
+        }},"hexcasting:type":"hexcasting:pattern"}`
+        mapPatterns[op] = mapPatterns[op.path] = patternNBT
     }
-    const GetPlayerItem = ctx => {
-        /**@type {Player}*/
-        let player = GetPlayer(ctx)
-        if (player) return player.mainHandItem
-    }
-    const KWMap = {
-        me: '5B,4B,5B',
-        eye: '4B,4B',
-        foot: '2B,2B',
-        dir: '0B,4B',
+    // map per-world patterns
+    let server = Utils.server
+    let perWorldMap = PRClass.getPerWorldPatterns(server.getLevel('overworld'))
+    server.scheduleInTicks(10, () => {
+        for (let seq in perWorldMap) {
+            let pair = perWorldMap[seq]
+            let op = pair.first
+            let startDir = pair.second
+            let patternNBT = `{"hexcasting:data":{angles:[B;${seq2bytes(seq)}],start_dir:${
+                mapStartDir[startDir]
+            }},"hexcasting:type":"hexcasting:pattern"}`
+            mapPatterns[op] = mapPatterns[op.path] = patternNBT
+        }
+    })
 
-        // ray tracing
-        trace_block: '0B,5B,4B,4B,0B,2B,2B',
-        trace_offset: '0B,1B,2B,2B,0B,4B,4B',
-        trace_entity: '0B,1B,4B,5B,4B',
+    ServerEvents.commandRegistry(e => {
+        const { commands: cmd, arguments: arg } = e
+        const GetPlayer = ctx => {
+            /**@type {Player}*/
+            let player = ctx.source.entity
+            if (player && player.isPlayer()) return player
+        }
+        const TEMPLATES = {
+            pattern: '{"hexcasting:data":{angles:[B;@],start_dir:0b},"hexcasting:type":"hexcasting:pattern"}',
+            list: '{"hexcasting:data":[@],"hexcasting:type":"hexcasting:list"}',
+            num: '{"hexcasting:data":@,"hexcasting:type":"hexcasting:double"}',
+        }
 
-        // block op
-        break: '5B,4B,5B,5B,5B,5B,5B',
-        water: '4B,5B,4B,0B,5B,4B,2B,4B,5B',
-        boom: '4B,4B,0B,4B,4B,0B,4B,4B',
-        block: '5B,5B,4B',
+        e.register(
+            cmd.literal('hexParse').then(
+                cmd.argument('code', arg.STRING.create(e)).executes(ctx => {
+                    let code = []
+                    String(arg.STRING.getResult(ctx, 'code')).replace(/\(|\)|\[|\]|[\w\.\/]+/g, match => (code.push(match), ''))
 
-        // meta
-        escape: '5B,5B,5B,4B,0B',
-        '(': '5B,5B,5B',
-        ')': '1B,1B,1B',
-        read: '4B,5B,5B,5B,5B,5B',
-        write: '2B,1B,1B,1B,1B,1B',
+                    let stack = [[]]
+                    for (let kw of code) {
+                        // nested list
+                        if (kw === '[') {
+                            stack.unshift([])
+                        } else if (kw === ']') {
+                            let inner = stack.shift()
+                            stack[0].push(TEMPLATES.list.replace('@', inner.join(',')))
+                        }
 
-        dupe: '4B,4B,2B,4B,4B',
-        dupe2: '4B,4B,2B,4B,2B,4B,4B,0B',
-        dupen: '4B,4B,2B,4B,4B,2B,4B,4B',
-    }
-    const TEMPLATES = {
-        pattern: '{"hexcasting:data":{angles:[B;@],start_dir:0b},"hexcasting:type":"hexcasting:pattern"}',
-        list: '{"hexcasting:data":[@],"hexcasting:type":"hexcasting:list"}',
-        num: '{"hexcasting:data":@,"hexcasting:type":"hexcasting:double"}',
-    }
+                        // TODO num/vec literals
+                        // TODO num pattern
 
-    e.register(
-        cmd.literal('hexParse').then(
-            cmd.argument('code', arg.STRING.create(e)).executes(ctx => {
-                let code = arg.STRING.getResult(ctx, 'code')
-                    .split(' ')
-                    .filter(x => x)
-
-                let stack = [[]]
-                for (let kw of code) {
-                    // nested list
-                    if (kw === '[') {
-                        stack.unshift([])
-                    } else if (kw === ']') {
-                        let inner = stack.shift()
-                        stack[0].push(TEMPLATES.list.replace('@', inner.join(',')))
+                        // normal kw
+                        else if (kw in mapPatterns) {
+                            stack[0].push(mapPatterns[kw])
+                        } else {
+                            Utils.server.tell(`unknown keyword: ${kw}`)
+                        }
                     }
 
-                    // TODO num/vec literals
-                    // TODO num pattern
-
-                    // normal kw
-                    else if (kw in KWMap) {
-                        stack[0].push(TEMPLATES.pattern.replace('@', KWMap[kw]))
-                    } else {
-                        Utils.server.tell(`unknown keyword: ${kw}`)
+                    // parse to item
+                    let player = GetPlayer(ctx)
+                    let target = null
+                    if (player.mainHandItem.id == 'hexcasting:focus') target = player.mainHandItem
+                    else if (player.offhandItem.id == 'hexcasting:focus') target = player.offhandItem
+                    if (target) {
+                        let fooItem = Item.of('hexcasting:focus', `{data:${TEMPLATES.list.replace('@', stack[0].join(','))}}`)
+                        target.orCreateTag.data = fooItem.nbt.data
                     }
-                }
-
-                // parse to item
-                let player = GetPlayer(ctx)
-                let target = null
-                if (player.mainHandItem.id == 'hexcasting:focus') target = player.mainHandItem
-                else if (player.offhandItem.id == 'hexcasting:focus') target = player.offhandItem
-                if (target) {
-                    let fooItem = Item.of('hexcasting:focus', `{data:${TEMPLATES.list.replace('@', stack[0].join(','))}}`)
-                    target.orCreateTag.data = fooItem.nbt.data
-                }
-                return 114514
-            }),
-        ),
-    )
-})
+                    return 114514
+                }),
+            ),
+        )
+    })
+}
