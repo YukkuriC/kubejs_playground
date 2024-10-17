570775785cd79017274e9137de8a3f67d244d0fd
YukkuriC <799433638@qq.com>
2024-10-18 00:33:35 +0800

    kjs仅1,20需继承一层访问

--- startup_scripts/reflection/ClassFilterUnlocker.js
+++ startup_scripts/reflection/ClassFilterUnlocker.js
@@ -2,7 +2,7 @@
     let ClassFilter = Java.loadClass('dev.latvian.mods.kubejs.util.ClassFilter')
     global.unlockClassFilter = (javaWrapper, superCount) => {
         let sm = global.getField(javaWrapper, 'manager')
-        global.setField(sm, 'classFilter', ClassFilter(), 0, sm.scriptType == 'SERVER')
+        global.setField(sm, 'classFilter', ClassFilter(), 0, sm.scriptType == 'SERVER' && Platform.getMcVersion() >= '1.20')
     }
 
     global.unlockClassFilter(Java)
