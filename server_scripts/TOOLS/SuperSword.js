5d155670ffaef10f50ee04612b1fc1f6f5f330fd
YukkuriC <799433638@qq.com>
2024-11-07 22:40:20 +0800

    优化吸血功能

--- server_scripts/TOOLS/SuperSword.js
+++ server_scripts/TOOLS/SuperSword.js
@@ -16,17 +16,35 @@ EntityEvents.hurt(e => {
     const { entity, source } = e
     const { player } = source
     if (player?.mainHandItem?.id != 'yc:sword') return
-    const before = entity.maxHealth
-    let after = Math.ceil(entity.maxHealth / 2)
-    let target = after
-    for (let i = 0; i < 100; i++) {
-        entity.setMaxHealth(after)
-        if (entity.maxHealth <= target) break
-        after = Math.ceil(after / 2)
-    }
-    entity.invulnerableTime = 0
-    const delta = Math.abs(before - entity.maxHealth)
+    // drain max health
+    const before = entity.getAttributeBaseValue('generic.max_health')
+    entity.setAttributeBaseValue('generic.max_health', Math.ceil(before / 2))
+    // less invulnerable
+    entity.invulnerableTime = Math.max(entity.invulnerableTime, 3)
+    // boost health
+    const delta = before / 2
+    let existBoost = player
+        .getAttribute('generic.max_health')
+        .getModifiers()
+        .find(x => x.name == 'yc:sword_vampire')
+    if (delta > (existBoost?.amount || 0)) player.modifyAttribute('minecraft:generic.max_health', 'yc:sword_vampire', delta, 'addition')
     player.heal(delta)
-    player.modifyAttribute('minecraft:generic.max_health', 'yc:sword_vampire', delta, 'addition')
-    if (player.shiftKeyDown) e.cancel()
+})
+
+PlayerTickEvents.every(40).on(e => {
+    let { player } = e
+    let healthBoost = player
+        .getAttribute('generic.max_health')
+        .getModifiers()
+        .find(x => x.name == 'yc:sword_vampire')
+    global.test = healthBoost
+    if (healthBoost) {
+        let val = healthBoost.amount
+        if (val > 80) val -= 20
+        else if (val > 40) val -= 10
+        else if (val > 20) val -= 5
+        else val = Math.max(0, val - 2)
+        player.modifyAttribute('minecraft:generic.max_health', 'yc:sword_vampire', val, 'addition')
+        player.heal(0.01)
+    }
 })
