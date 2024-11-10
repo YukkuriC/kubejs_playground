e39bf7a8dacb4d93d1cea0cbae3cb7e4fbc43dd2
YukkuriC <799433638@qq.com>
2024-11-10 21:34:07 +0800

    不连锁处于无敌状态对象

--- server_scripts/TOOLS/SuperSword.js
+++ server_scripts/TOOLS/SuperSword.js
@@ -84,7 +84,7 @@ EntityEvents.hurt(e => {
         level.server.scheduleInTicks(Math.random() * 10 + 4, () => {
             let cnt = 0
             for (let sube of level.getEntitiesWithin(entity.boundingBox.inflate(10))) {
-                if (!sube.isLiving() || !sube.isAlive() || sube === entity || sube === player) continue
+                if (!sube.isLiving() || !sube.isAlive() || sube === entity || sube === player || sube.invulnerableTime > 0) continue
                 let subPos = sube.eyePosition
                 sube.attack(source, e.damage)
                 server.sendData('yc:sword_line', {
