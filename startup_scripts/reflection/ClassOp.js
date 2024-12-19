9397f8a1f8f6b7671db412dc750b988437af3d29
YukkuriC <799433638@qq.com>
2024-12-19 23:18:04 +0800

    reduce chain

--- startup_scripts/reflection/ClassOp.js
+++ startup_scripts/reflection/ClassOp.js
@@ -4,4 +4,4 @@ global.toRawClass = cls => {
     let clsName = String(cls).match(/[\w\.]+(?=\]?$)/)[0]
     return global.loadRawClass(clsName)
 }
-global.loadRawClass = clsName => Java.class.class.forName(clsName)
+global.loadRawClass = clsName => Java.class.forName(clsName)
