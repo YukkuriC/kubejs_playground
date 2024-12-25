b674a84708d038993b130d50b813484f64dc78ca
YukkuriC <799433638@qq.com>
2024-12-25 19:45:28 +0800

    event重构

--- server_scripts/libs/Event.js
+++ server_scripts/libs/Event.js
@@ -30,29 +30,10 @@
             for (let f of this.__onceSet) f.apply(null, args)
             this.__onceSet.clear()
         },
+        fire() {
+            return this.fireVar(Array.from(arguments))
+        },
     }
-
-    // 定参数
-    let args = []
-    let i = -1
-    for (; i <= 3; i++) {
-        if (i >= 0) args.push('a' + i)
-        Event.prototype[`fire${args.length}`] = eval(`function(${args.join(',')}){
-            for (let f of this.__set) f(${args})
-            for (let f of this.__onceSet) f(${args})
-            this.__onceSet.clear()
-        }`)
-    }
-    // 变长判断参数
-    for (; i < 10; i++) args.push('a' + i)
-    Event.prototype[`fire`] = eval(`function(${args.join(',')}){
-        let argsHack = [${args.join(',')}]
-        for (let i = ${args.length} - 1; i >= 0; i--) {
-            if(argsHack[i] === undefined) argsHack.pop()
-            else break
-        }
-        return this.fireVar(argsHack)
-    }`)
 
     this.Event = global.Event = Event
 }
