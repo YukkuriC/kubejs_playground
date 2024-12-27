c0fc6ac1e90599d8d001ec931157ae3ad22aa1e1
YukkuriC <799433638@qq.com>
2024-12-27 13:37:08 +0800

    超级信标

---
+++ startup_scripts/ExtendedBeacon.js
@@ -0,0 +1,30 @@
+{
+    let BeaconBlockEntity = Java.loadClass('net.minecraft.world.level.block.entity.BeaconBlockEntity')
+    let AllEffects = Java.loadClass('net.minecraft.core.registries.BuiltInRegistries').MOB_EFFECT
+    let HashSet = Java.loadClass('java.util.HashSet')
+
+    StartupEvents.postInit(e => {
+        let original = BeaconBlockEntity.BEACON_EFFECTS
+        let mutable = []
+        for (let i = 0; i < 4; i++) mutable[i] = Array.from(original[i])
+
+        // add effects
+        mutable[0].push(AllEffects.get('night_vision'))
+        mutable[1].push(AllEffects.get('bad_omen'))
+        if (Platform.isLoaded('hexcasting')) {
+            mutable[2].push(AllEffects.get('hexcasting:enlarge_grid'))
+        }
+        if (Platform.isLoaded('ars_nouveau')) {
+            mutable[2].push(AllEffects.get('ars_nouveau:mana_regen'))
+        }
+
+        // update effects back
+        for (let i = 0; i < 4; i++) original[i] = mutable[i]
+
+        // update validation
+        let flattened = []
+        for (let sub of mutable) flattened.push.apply(flattened, sub)
+        flattened = new HashSet(flattened)
+        global.unsafeSetField(BeaconBlockEntity, /*VALID_EFFECTS*/ 'f_58647_', flattened, 1)
+    })
+}
--- startup_scripts/reflection/FieldOp.js
+++ startup_scripts/reflection/FieldOp.js
@@ -16,3 +16,14 @@ global.getField = (obj, name, isStatic, superCount) => {
 global.setField = (obj, name, value, isStatic, superCount) => {
     return global.getDeclaredField(obj, name, isStatic, superCount).set(obj, value)
 }
+
+// UNSAFE
+{
+    let unsafe = global.getField('sun.misc.Unsafe', 'theUnsafe', 1)
+    global.unsafeSetField = (obj, name, value, isStatic, superCount) => {
+        let field = global.getDeclaredField(obj, name, isStatic, superCount)
+        let base = isStatic ? unsafe.staticFieldBase(field) : obj
+        let offset = isStatic ? unsafe.staticFieldOffset(field) : unsafe.objectFieldOffset(field)
+        unsafe.putObject(base, offset, value)
+    }
+}
