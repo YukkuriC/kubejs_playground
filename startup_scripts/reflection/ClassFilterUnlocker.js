c035e31257d8dcdd4b9971ee07f7ecf1bb7b8e23
YukkuriC <799433638@qq.com>
2025-01-14 23:10:53 +0800

    remove unused param

--- startup_scripts/reflection/ClassFilterUnlocker.js
+++ startup_scripts/reflection/ClassFilterUnlocker.js
@@ -1,6 +1,6 @@
 {
     let ClassFilter = Java.loadClass('dev.latvian.mods.kubejs.util.ClassFilter')
-    global.unlockClassFilter = (javaWrapper, superCount) => {
+    global.unlockClassFilter = (javaWrapper) => {
         let sm = global.getField(javaWrapper, 'manager')
         global.setField(sm, 'classFilter', ClassFilter(), 0, sm.scriptType == 'SERVER' && Platform.getMcVersion() >= '1.20')
     }
