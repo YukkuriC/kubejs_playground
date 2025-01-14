23824a57edbd90315ba71f7d63b0b57392f5224f
YukkuriC <799433638@qq.com>
2025-01-14 22:26:17 +0800

    reflection crack TODO: 出的bug有点抽象，再瞅瞅

--- startup_scripts/reflection/NashornCrack.js
+++ startup_scripts/reflection/NashornCrack.js
@@ -1,12 +1,20 @@
-global.Nashorn = global.Nashorn || Java.loadClass('javax.script.ScriptEngineManager')().getEngineByName('nashorn')
+const NashornInit = !global.Nashorn
 
 // classfilter breaker
-global.Nashorn.eval(`
-var F_SM_CF = Java.type('dev.latvian.mods.kubejs.script.ScriptManager').class.getDeclaredField('classFilter')
-F_SM_CF.setAccessible(true)
-var CF = Java.type('dev.latvian.mods.kubejs.plugin.ClassFilter')
+const code1 = `
+function getField(cls, name) {
+    var field = getCls(cls).class.getDeclaredField(name)
+    field.setAccessible(true)
+    return field
+}
+function getCls(cls) {
+    return Java.type('dev.latvian.mods.kubejs.' + cls)
+}
+
+var F_SM_CF = getField('script.ScriptManager', 'classFilter')
+var CF = getCls('plugin.ClassFilter')
 var cf = new CF(null)
-var KJS = Java.type('dev.latvian.mods.kubejs.KubeJS')
+var KJS = getCls('KubeJS')
 var smMap = {}
 
 function unlock(sm, type) {
@@ -19,4 +27,61 @@ function unlockServer(server) {
 }
 
 unlock(KJS.startupScriptManager, 'startup')
-`)
+`
+
+// reflection unlocker
+const code2 = `
+var PluginList = getField('plugin.KubeJSPlugins', 'LIST').get(null)
+var KJSPlugin = getCls('plugin.KubeJSPlugin')
+var KJSContext = getCls('script.KubeJSContext')
+var RhinoContext = Java.type('dev.latvian.mods.rhino.Context')
+var KJSFactory = getCls('script.KubeJSContextFactory')
+var AccessibleObject = Java.type('java.lang.reflect.AccessibleObject')
+var ClassLoader = Java.type('java.lang.ClassLoader')
+
+var F_TW_T = getField('script.TypeWrapperRegistry', 'type')
+
+var fooFactory = null
+var HackedPlugin = Java.extend(KJSPlugin, {
+    registerTypeWrappers: function (wrapper) {
+        var type = F_TW_T.get(wrapper)
+        var sm = smMap[type.name]
+        if (!sm) return
+        if (!fooFactory) fooFactory = new KJSFactory(sm)
+        var wrapFactory = new HackedFactory(sm)
+        type.console.contextFactory.refersTo(wrapFactory)
+        sm.contextFactory = wrapFactory
+    },
+})
+var HackedFactory = Java.extend(KJSFactory, {
+    createContext: function () {
+        var fooContext = new RhinoContext(fooFactory)
+        var wrapContext = new HackedContext(fooFactory, {
+            wrapAsJavaObject: function (scope, obj, target) {
+                if (obj instanceof AccessibleObject || obj instanceof ClassLoader) {
+                    return fooContext.wrapAsJavaObject(scope, obj, target)
+                }
+                return suu.wrapAsJavaObject(scope, obj, target)
+            },
+            // internalJsToJavaLast: function (from, target) {
+            //     var c = target.asClass()
+            //     if (AccessibleObject.class.isAssignableFrom(c) || ClassLoader.class.isAssignableFrom(c)) {
+            //         return fooContext.internalJsToJavaLast(from, target)
+            //     }
+            //     return suu.internalJsToJavaLast(from, target)
+            // },
+        })
+        var suu = Java.super(wrapContext)
+        return wrapContext
+    },
+})
+var HackedContext = Java.extend(KJSContext)
+
+PluginList.add(new HackedPlugin())
+`
+
+if (NashornInit) {
+    global.Nashorn = Java.loadClass('javax.script.ScriptEngineManager')().getEngineByName('nashorn')
+    global.Nashorn.eval(code1)
+    global.Nashorn.eval(code2)
+}
