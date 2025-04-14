f5639564d0d82380368ea3e32274f325b35992ba
YukkuriC <799433638@qq.com>
2025-04-14 23:33:48 +0800

    shared rage & adjust spell list

--- server_scripts/misc/VillagersSupe.js
+++ server_scripts/misc/VillagersSupe.js
@@ -5,40 +5,58 @@
     let AttackSpells = [
         // rand pool
         SpellRegistry.MAGIC_MISSILE_SPELL,
-        SpellRegistry.LOB_CREEPER_SPELL,
-        SpellRegistry.FIREBOLT_SPELL,
-        SpellRegistry.FIRECRACKER_SPELL,
+        // SpellRegistry.LOB_CREEPER_SPELL,
+        // SpellRegistry.FIREBOLT_SPELL,
+        // SpellRegistry.FIRECRACKER_SPELL,
         SpellRegistry.ELDRITCH_BLAST_SPELL,
-        SpellRegistry.BLOOD_NEEDLES_SPELL,
+        SpellRegistry.ACUPUNCTURE_SPELL,
     ]
+
+    let fightBackTargetInvalidCheckLive = (entity, actual) => actual.health <= 0 || actual.position().distanceTo(entity.position()) > 30
+    let fightBackTargetInvalidCheck = (entity, actual) =>
+        actual == null ||
+        actual.type === entity.type ||
+        (actual.isPlayer() && (actual.creative || actual.spectator)) ||
+        fightBackTargetInvalidCheckLive(entity, actual)
 
     EntityEvents.hurt('villager', ev => {
         let {
             entity,
+            level,
             source: { actual },
-            server,
         } = ev
 
+        villagerFightBack(entity, actual)
+        if (actual == null) return
+        for (let nearby of level.getEntitiesWithin(actual.boundingBox.inflate(20))) {
+            if (nearby.type !== entity.type) continue
+            villagerFightBack(nearby, actual)
+        }
+    })
+
+    /**
+     * @param {Internal.Villager} entity
+     * @param {Internal.Entity} actual
+     * @param {number} angerLevel
+     */
+    let villagerFightBack = (entity, actual) => {
+        let { server } = entity
+
+        // no too frequent trigger
         let { lastAttackTick } = entity.persistentData
         let nowTick = server.tickCount
         if (nowTick - (lastAttackTick || 0) < 10) return
         entity.persistentData.lastAttackTick = nowTick
-        let anger = 10
+        let anger = 10,
+            targetInvalidChecker = fightBackTargetInvalidCheck
 
         // modules
         let invalidCheck = () => {
             return entity.health <= 0 || entity.persistentData.lastAttackTick !== nowTick
         }
         let tryAttack = () => {
-            if (invalidCheck()) return
-            if (
-                actual == null ||
-                actual.type === entity.type ||
-                actual.health <= 0 ||
-                (actual.isPlayer() && (actual.creative || actual.spectator)) ||
-                actual.pos.distanceTo(entity.pos) > 30
-            )
-                return
+            if (invalidCheck() || targetInvalidChecker(entity, actual)) return
+            targetInvalidChecker = fightBackTargetInvalidCheckLive
             entity.target = actual
             for (let i = 2; i <= 10; i += 2)
                 entity.server.scheduleInTicks(i, () => {
@@ -68,5 +86,5 @@
 
         // attack & heal
         if (!tryAttack()) server.scheduleInTicks(1, tryHeal)
-    })
+    }
 }
