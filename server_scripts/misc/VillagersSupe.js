4fe8ff43df7228a06fb782ba156cd38c160a8c7a
YukkuriC <799433638@qq.com>
2025-04-14 21:58:49 +0800

    modular attacker

--- server_scripts/misc/VillagersSupe.js
+++ server_scripts/misc/VillagersSupe.js
@@ -15,24 +15,42 @@
         let {
             entity,
             source: { actual },
+            server,
         } = ev
+
+        let { lastAttackTick } = entity.persistentData
+        let nowTick = server.tickCount
+        if (nowTick - (lastAttackTick || 0) < 10) return
+        entity.persistentData.lastAttackTick = nowTick
+
+        // modules
+        let invalidCheck = () => {
+            return entity.health <= 0 || entity.persistentData.lastAttackTick !== nowTick
+        }
+        let tryAttack = () => {
+            if (invalidCheck()) return
+            if (actual == null || actual.type === entity.type || (actual.isPlayer() && (actual.creative || actual.spectator))) return
+            entity.target = actual
+            for (let i = 2; i <= 10; i += 2)
+                entity.server.scheduleInTicks(i, () => {
+                    if (invalidCheck()) return
+                    entity.initiateCastSpell(AttackSpells[Math.floor(Math.random() * AttackSpells.length)].get(), 10)
+                })
+
+            // heal after attack
+            server.scheduleInTicks(11, tryHeal)
+            return true
+        }
+        let tryHeal = () => {
+            if (invalidCheck() || entity.maxHealth === entity.health) return
+            entity.target = null
+            entity.initiateCastSpell(SpellRegistry.CLOUD_OF_REGENERATION_SPELL.get(), 10)
+        }
 
         // buff
         entity.initiateCastSpell(SpellRegistry.OAKSKIN_SPELL.get(), 10)
 
-        // attack
-        if (actual != null && actual.type !== entity.type) {
-            entity.target = actual
-            for (let i = 2; i <= 10; i += 2)
-                entity.server.scheduleInTicks(i, () => {
-                    entity.initiateCastSpell(AttackSpells[Math.floor(Math.random() * AttackSpells.length)].get(), 10)
-                })
-        }
-
-        // heal
-        entity.server.scheduleInTicks(11, () => {
-            if (entity.maxHealth != entity.health) return
-            entity.initiateCastSpell(SpellRegistry.CLOUD_OF_REGENERATION_SPELL.get(), 10)
-        })
+        // attack & heal
+        if (!tryAttack()) server.scheduleInTicks(1, tryHeal)
     })
 }
