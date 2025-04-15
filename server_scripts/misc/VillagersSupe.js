84ab63999c9f96d43086e622191bf78b44e26fe0
YukkuriC <799433638@qq.com>
2025-04-15 22:39:53 +0800

    bugfix & force cast method

--- server_scripts/misc/VillagersSupe.js
+++ server_scripts/misc/VillagersSupe.js
@@ -10,6 +10,7 @@
         // SpellRegistry.FIRECRACKER_SPELL,
         SpellRegistry.ELDRITCH_BLAST_SPELL,
         SpellRegistry.ACUPUNCTURE_SPELL,
+        SpellRegistry.TELEPORT_SPELL,
     ]
 
     let fightBackTargetInvalidCheckLive = (entity, actual) => actual.health <= 0
@@ -18,6 +19,26 @@
         actual.type === entity.type ||
         (actual.isPlayer() && (actual.creative || actual.spectator)) ||
         fightBackTargetInvalidCheckLive(entity, actual)
+    /**
+     * @param {Internal.Villager} entity
+     * @param {Internal.RegistryObject<Internal.AbstractSpell>} spellHolder
+     */
+    let forceCast = (entity, spellHolder, target) => {
+        let canForceCast = spellHolder !== SpellRegistry.TELEPORT_SPELL // and blood step, etc.
+        let spell = spellHolder.get()
+        canForceCast &= spell.castType != 'continuous'
+
+        if (canForceCast) {
+            let { magicData, level } = entity
+            if (target) {
+                magicData.additionalCastData = TargetEntityCastData(target)
+                entity.lookAt(target, 180, 180)
+            }
+            spell.onCast(level, spell.maxLevel, entity, 'mob', magicData)
+        } else {
+            entity.initiateCastSpell(spell, spell.maxLevel)
+        }
+    }
 
     /**
      * @param {Internal.Villager} entity
@@ -29,7 +50,7 @@
 
         // no too frequent trigger
         let { lastAttackTick } = entity.persistentData
-        let nowTick = server.tickCount
+        let nowTick = entity.age // tickCount clears after restart
         if (nowTick - (lastAttackTick || 0) < 10) return
         entity.persistentData.lastAttackTick = nowTick
         let anger = 10,
@@ -46,11 +67,11 @@
             for (let i = 2; i <= 10; i += 2)
                 entity.server.scheduleInTicks(i, () => {
                     if (invalidCheck()) return
-                    entity.initiateCastSpell(AttackSpells[Math.floor(Math.random() * AttackSpells.length)].get(), 10)
+                    forceCast(entity, AttackSpells[Math.floor(Math.random() * AttackSpells.length)], actual)
                 })
 
             // heal after attack
-            server.scheduleInTicks(11, tryHeal)
+            server.scheduleInTicks(11, tryHealAndEnd)
 
             // repeat attack
             if (anger > 0) {
@@ -60,17 +81,19 @@
 
             return true
         }
-        let tryHeal = () => {
+        let tryHealAndEnd = () => {
             if (invalidCheck() || entity.maxHealth === entity.health) return
             entity.target = null
-            entity.initiateCastSpell(SpellRegistry.CLOUD_OF_REGENERATION_SPELL.get(), 10)
+            forceCast(entity, SpellRegistry.CLOUD_OF_REGENERATION_SPELL)
+            if (entity.health / entity.maxHealth < 0.5) forceCast(entity, SpellRegistry.EVASION_SPELL)
         }
 
         // buff
-        entity.initiateCastSpell(SpellRegistry.OAKSKIN_SPELL.get(), 10)
+        forceCast(entity, SpellRegistry.OAKSKIN_SPELL)
+        forceCast(entity, SpellRegistry.CHARGE_SPELL)
 
         // attack & heal
-        if (!tryAttack()) server.scheduleInTicks(1, tryHeal)
+        if (!tryAttack()) server.scheduleInTicks(1, tryHealAndEnd)
     }
 
     EntityEvents.hurt('villager', ev => {
