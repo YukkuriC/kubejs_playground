93de9d8cbb16124be1801fc1381b913bf648d6b7
YukkuriC <799433638@qq.com>
2025-06-20 22:17:10 +0800

    灵魂方舟快递

--- startup_scripts/goety/extra_caps.js
+++ startup_scripts/goety/extra_caps.js
@@ -2,6 +2,7 @@
 {
     let ArcaBlockEntity = Java.loadClass('com.Polarice3.Goety.common.blocks.entities.ArcaBlockEntity')
     let IEnergyStorage = Java.loadClass('net.minecraftforge.energy.IEnergyStorage')
+    let IItemHandler = Java.loadClass('net.minecraftforge.items.IItemHandler')
     let { ENERGY: FECap, ITEM_HANDLER: ItemCap } = Java.loadClass('net.minecraftforge.common.capabilities.ForgeCapabilities')
     let SEHelper = Java.loadClass('com.Polarice3.Goety.utils.SEHelper')
     let MainConfig = Java.loadClass('com.Polarice3.Goety.config.MainConfig')
@@ -9,6 +10,7 @@
 
     let RATE = 200
     let RATE_EXTRACT_LOSS = 0.1
+    /**@type {Internal.IEnergyStorage & {arca:Internal.ArcaBlockEntity}}*/
     let protoFE = {
         getSoul() {
             if (!this.arca.player) return 0
@@ -55,18 +57,57 @@
         },
     }
 
+    /**@type {Internal.IItemHandler & {arca:Internal.ArcaBlockEntity}}*/
+    let protoItem = {
+        getSlots() {
+            let inv = this.arca.player?.inventory
+            if (!inv) return 0
+            return inv.slots
+        },
+        getStackInSlot(slot) {
+            let inv = this.arca.player?.inventory
+            if (!inv) return Item.empty
+            return inv.getStackInSlot(slot)
+        },
+        insertItem(slot, stack, simulate) {
+            let player = this.arca.player
+            if (!player) return stack
+            if (!simulate) player.give(stack)
+            return Item.empty
+        },
+        extractItem(slot, amount, simulate) {
+            return Item.empty
+        },
+        getSlotLimit(slot) {
+            return 114514
+        },
+        isItemValid(slot, stack) {
+            return true
+        },
+    }
+
     let doArcaInject = (/**@type {Internal.AttachCapabilitiesEvent<Internal.BlockEntity>}*/ event) => {
         let be = event.getObject()
         try {
             if (!(be instanceof ArcaBlockEntity)) return
             event.addCapability('kubejs:arca_fe', (cap, side) => {
-                if (cap !== FECap) return LazyOptional.empty()
-                return LazyOptional.of(() => {
-                    return new JavaAdapter(IEnergyStorage, {
-                        arca: be,
-                        __proto__: protoFE,
-                    })
-                })
+                if (cap == FECap)
+                    return LazyOptional.of(
+                        () =>
+                            new JavaAdapter(IEnergyStorage, {
+                                arca: be,
+                                __proto__: protoFE,
+                            }),
+                    )
+                else if (cap == ItemCap)
+                    return LazyOptional.of(
+                        () =>
+                            new JavaAdapter(IItemHandler, {
+                                arca: be,
+                                __proto__: protoItem,
+                            }),
+                    )
+                return LazyOptional.empty()
             })
         } catch (e) {
             if (Utils.server) Utils.server.tell(e)
