5d0e5d61fc72302bf0aec75d08eef2589f458e9e
YukkuriC <799433638@qq.com>
2025-07-26 21:27:22 +0800

    simplify code

--- server_scripts/MNA/CustomRecipes.js
+++ server_scripts/MNA/CustomRecipes.js
@@ -2,7 +2,7 @@
 ServerEvents.recipes(e => {
     // auto rune patterns
     e.forEachRecipe({ type: 'mna:runescribing' }, recipe => {
-        let target = recipe.json.asJsonObject.get('output').getAsString()
+        let target = recipe.json.get('output').asString
         e.shapeless(target, [
             Item.of('mna:runescribing_recipe_paper', { runescribe_recipe_id: String(recipe.id) }).strongNBT(),
             'blaze_rod',
