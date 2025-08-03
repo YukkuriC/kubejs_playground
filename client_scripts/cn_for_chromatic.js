bf2368f66bb2d99d71d3feebd24e7eb0fcc6d83c
YukkuriC <799433638@qq.com>
2025-08-03 14:17:07 +0800

    disable unused script

--- client_scripts/cn_for_chromatic.js
+++ client_scripts/cn_for_chromatic.js
@@ -1,3 +1,5 @@
+// ignored: true
+// 暴力替换汉化，但是要汉化的mod芜了
 let langMap = JsonIO.read('kubejs/assets/createchromaticreturn/lang/zh_cn.json')
 ItemEvents.tooltip(e => {
     for (let item of Item.list) {
