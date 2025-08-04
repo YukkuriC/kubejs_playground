b4f4ed225b02038cc43c172fe43c31028555999d
YukkuriC <799433638@qq.com>
2025-08-04 22:31:09 +0800

    drawer compat follow version

--- startup_scripts/misc/SuperFarDrawers.js
+++ startup_scripts/misc/SuperFarDrawers.js
@@ -1,10 +1,15 @@
 // requires: storagedrawers
-// working on 1.20.1-12.9.14
+// working on 1.20.1-12.9.14 & 1.20.1-12.11.2
 {
     let Integer = Java.loadClass('java.lang.Integer')
-    let CFG = Java.loadClass('com.jaquadro.minecraft.storagedrawers.config.CommonConfig')
+    let CFG
+    try {
+        CFG = Java.tryLoadClass('com.jaquadro.minecraft.storagedrawers.config.ModCommonConfig')
+    } catch (e) {
+        CFG = { INSTANCE: Java.loadClass('com.jaquadro.minecraft.storagedrawers.config.CommonConfig') }
+    }
 
     StartupEvents.postInit(() => {
-        CFG.GENERAL.controllerRange.set(Integer('114514'))
+        CFG.INSTANCE.GENERAL.controllerRange.set(Integer('114514'))
     })
 }
