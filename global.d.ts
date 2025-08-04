0eea5865ad35a957bb6d24e1bab091885f8f09e9
YukkuriC <799433638@qq.com>
2025-08-05 00:21:36 +0800

    adapter d.ts update

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,13 @@
 type Super$<T> = `super$${T}`
 type AdapterPrefixed<T> = { [k in keyof T as Super$<k>]: T[k] }
 type AdapterMerged<T> = T & AdapterPrefixed<T>
+type MergedTuple<L extends unknown[], RET = {}> = L['length'] extends 0
+    ? RET
+    : L extends [infer H, ...infer L2]
+    ? MergedTuple<L2, RET & H>
+    : never
 
 type Cls2Obj<CT> = CT extends { new (...args: unknown[]): infer T } ? T : never
-class JavaAdapter<CT, F, U = AdapterMerged<Cls2Obj<CT>>, R = F & U> extends R {
-    constructor(src: CT, overrides: R, ...args: ConstructorParameters<CT>)
+class JavaAdapter<CT, IT extends unknown[], F, U = AdapterMerged<Cls2Obj<CT> & MergedTuple<IT>>, R = F & U> extends R {
+    constructor(src: CT, /* ...interfaces: IT, TODO: support multi-interface impl. */ overrides: R, ...args: ConstructorParameters<CT>)
 }
