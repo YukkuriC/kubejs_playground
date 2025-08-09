f326a7432dc71858365bd3c9d0e2f9aa67aac876
YukkuriC <799433638@qq.com>
2025-08-09 22:55:09 +0800

    old-fashioned wellspring charging

---
+++ server_scripts/MNA/WellspringRecharging.js
@@ -0,0 +1,28 @@
+{
+    let WorldMagicProvider = Java.loadClass('com.mna.capabilities.worlddata.WorldMagicProvider')
+
+    let CHECK_INTERVAL = 20
+    let ADD_SIZE = 10
+    let AFFINITY_TARGETS = ['ARCANE', 'EARTH', 'ENDER', 'FIRE', 'WATER', 'WIND']
+
+    ServerEvents.tick(e => {
+        try {
+            let { server } = e
+            if (server.tickCount % CHECK_INTERVAL) return
+            let level = server.overworld()
+            let players = server.players
+            level.getCapability(WorldMagicProvider.MAGIC).ifPresent(m => {
+                let powers = m.wellspringRegistry
+                for (let player of server.players) {
+                    let { uuid } = player
+                    for (let affinity of AFFINITY_TARGETS) {
+                        let mult = powers.getEldrinGenerationMultiplierFor(uuid, level, affinity) - 1
+                        if (mult >= 0) powers.insertPower(uuid, level, affinity, mult * ADD_SIZE)
+                    }
+                }
+            })
+        } catch (e) {
+            Client.player.tell(e)
+        }
+    })
+}
