4c501ddb99a91c603974f310b351f2912d0c0b05
YukkuriC <799433638@qq.com>
2025-10-20 21:16:16 +0800

    remove useless auto-wrap

--- server_scripts/MNA/magichem/Orrrrrrery.js
+++ server_scripts/MNA/magichem/Orrrrrrery.js
@@ -1,14 +1,6 @@
 // requires: magichem
 {
     let EldrinOrreryLimiterSD = Java.loadClass('com.aranaira.magichem.foundation.saveddata.EldrinOrreryLimiterSD')
-    let Function = Java.loadClass('java.util.function.Function')
-
-    let wrapInterface = func =>
-        new JavaAdapter(Function, {
-            apply(obj) {
-                return func(obj)
-            },
-        })
 
     BlockEvents.placed('magichem:eldrin_orrery', e => {
         let { player, server } = e
@@ -16,7 +8,7 @@
 
         try {
             let ds = server.overworld().dataStorage
-            let limiter = ds.get(wrapInterface(EldrinOrreryLimiterSD.load), 'eldrinOrreryData')
+            let limiter = ds.get(EldrinOrreryLimiterSD.load, 'eldrinOrreryData')
             limiter['removeOrrery(net.minecraft.world.entity.player.Player)'](player)
         } catch (e) {
             player.tell(e)
