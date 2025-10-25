7b23bc57982234b48beae7cd79013bf6f96efde7
YukkuriC <799433638@qq.com>
2025-10-25 23:11:27 +0800

    add mod limit

--- server_scripts/MNA/magichem/ColoringRecipes.js
+++ server_scripts/MNA/magichem/ColoringRecipes.js
@@ -1,3 +1,4 @@
+// requires: magichem
 {
     let ALL_COLORS = [
         'red',
--- server_scripts/MNA/magichem/CustomAlcohol.js
+++ server_scripts/MNA/magichem/CustomAlcohol.js
@@ -1,3 +1,4 @@
+// requires: magichem
 ServerEvents.recipes(e => {
     let helper = (id, count) =>
         e.custom({
