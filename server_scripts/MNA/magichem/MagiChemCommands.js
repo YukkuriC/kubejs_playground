d842273259aa55c1b026aa82c1432c9606fa8a5b
YukkuriC <799433638@qq.com>
2026-02-04 00:14:44 +0800

    magichem distillation recipe gen

---
+++ server_scripts/MNA/magichem/MagiChemCommands.js
@@ -0,0 +1,82 @@
+// requires: magichem
+ServerEvents.commandRegistry(e => {
+    let { commands: cmd, arguments: arg } = e
+
+    let pickMateria = name => {
+        if (name in pickMateria.cache) return pickMateria.cache[name]
+        for (let target of ['essentia', 'admixture']) {
+            let key = `magichem:${target}_${name}`
+            if (Item.of(key).empty) continue
+            return (pickMateria.cache[name] = key)
+        }
+        stopWith(ctx, 'invalid materia: ' + name)
+    }
+    pickMateria.cache = {}
+    let stopWith = (ctx, msg) => {
+        ctx.source.sendFailure(msg)
+        throw msg
+    }
+
+    // 1. distillation recipe gen
+    let raw_arg_descrip = 'CATEGORIES INV_RATE[/BATCH=1] WISDOM MatName[/Count=1],...'
+    let root = CommandUtils.chain(
+        [
+            cmd.literal('magichem'), //root command
+            cmd.literal('distill'),
+            cmd.argument(raw_arg_descrip, arg.GREEDY_STRING.create(e)),
+        ],
+        ctx => {
+            let handItem = CommandUtils.getPlayerItem(ctx)
+            if (!handItem || handItem.empty) stopWith(ctx, 'no item in player main hand')
+
+            let [
+                // no collapse
+                categories,
+                rawEff,
+                wisdom,
+                rawMats,
+            ] = arg.GREEDY_STRING.getResult(ctx, raw_arg_descrip).split(' ')
+            let [invRate, batch] = rawEff.split('/')
+            if (!batch) batch = 1
+
+            let recipe_body = {
+                type: 'magichem:distillation_fabrication',
+                wisdom: Number(wisdom),
+                categories: Number(categories),
+                output_rate: 1 / Number(invRate),
+                batch_size: Number(batch),
+                object: { item: handItem.id },
+                components: [],
+            }
+            // per materia
+            for (let raw of rawMats.split(',')) {
+                let [name, count] = raw.split('/')
+                if (!count) count = 1
+                recipe_body.components.push({ item: pickMateria(name), count: Number(count) })
+            }
+
+            let modid = handItem.idLocation.namespace
+            let wrapped = {
+                type: 'forge:conditional',
+                recipes: [
+                    {
+                        conditions: [
+                            {
+                                type: 'forge:mod_loaded',
+                                modid: modid,
+                            },
+                        ],
+                        recipe: recipe_body,
+                    },
+                ],
+            }
+
+            ctx.source.sendSuccess(Text.literal(`recipe for ${handItem.id} generated`), true)
+            JsonIO.write(`kubejs/data/magichem/recipes/distillation_fabrication/${modid}/${handItem.idLocation.path}.json`, wrapped)
+
+            return 0
+        },
+    )
+
+    e.register(root)
+})
