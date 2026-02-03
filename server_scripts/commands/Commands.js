da6850931ab8bef57b2bb2c6617e0a98087217a2
YukkuriC <799433638@qq.com>
2026-02-03 22:17:18 +0800

    commands refactor

--- server_scripts/commands/Commands.js
+++ server_scripts/commands/Commands.js
@@ -1,22 +1,6 @@
 ServerEvents.commandRegistry(e => {
     const { commands: cmd, arguments: arg } = e
 
-    const TryGetArg = (ctx, argType, name) => {
-        try {
-            return argType.getResult(ctx, name)
-        } catch (e) {}
-    }
-    const GetPlayer = ctx => {
-        /**@type {Player}*/
-        let player = ctx.source.entity
-        if (player && player.isPlayer()) return player
-    }
-    const GetPlayerItem = ctx => {
-        /**@type {Player}*/
-        let player = GetPlayer(ctx)
-        if (player) return player.getMainHandItem()
-    }
-
     // 玩家数据操作系列
     {
         let F = cmd.literal('F').requires(src => src.hasPermission(2))
@@ -25,18 +9,6 @@ ServerEvents.commandRegistry(e => {
         {
             let enchantKeys = ['Enchantments', 'StoredEnchantments']
 
-            let doEnchant = ctx => {
-                let item = GetPlayerItem(ctx)
-                if (!item) return 0
-
-                let type = arg.STRING.getResult(ctx, 'type')
-                if (type.indexOf(':') < 0) type = 'minecraft:' + type
-                let level = TryGetArg(ctx, arg.INTEGER, 'level') || 1
-                Utils.server.tell(`Enchanting ${type} Lv.${level}`)
-                if (item.id == 'minecraft:enchanted_book') item.getOrCreateTag().StoredEnchantments.push({ id: type, lvl: level })
-                else item.enchantStack(type, level)
-                return 1
-            }
             let getEnchantPool = (item, returnKey) => {
                 if (!item) return
                 let tag = item.getOrCreateTag()
@@ -44,41 +16,63 @@ ServerEvents.commandRegistry(e => {
                     if (tag[key] && tag[key].length > 0) return returnKey ? key : tag[key]
                 }
             }
-            F.then(
-                cmd.literal('enchant').then(
-                    cmd
-                        .argument('type', arg.STRING.create(e))
-                        .executes(doEnchant)
-                        .then(cmd.argument('level', arg.INTEGER.create(e)).executes(doEnchant)),
-                ),
-            ).then(
-                cmd
-                    .literal('enchant_remove')
-                    .executes(ctx => {
-                        let pool = getEnchantPool(GetPlayerItem(ctx))
+            CommandUtils.chain(
+                [
+                    F, // /F
+                    cmd.literal('enchant'),
+                    cmd.argument('type', arg.STRING.create(e)),
+                    cmd.argument('level', arg.INTEGER.create(e)),
+                ],
+                ctx => {
+                    let item = CommandUtils.getPlayerItem(ctx)
+                    if (!item) return 0
+
+                    let type = arg.STRING.getResult(ctx, 'type')
+                    if (type.indexOf(':') < 0) type = 'minecraft:' + type
+                    let level = CommandUtils.tryGetArg(ctx, arg.INTEGER, 'level') || 1
+                    Utils.server.tell(`Enchanting ${type} Lv.${level}`)
+                    if (item.id == 'minecraft:enchanted_book') item.getOrCreateTag().StoredEnchantments.push({ id: type, lvl: level })
+                    else item.enchantStack(type, level)
+                    return 1
+                },
+                2,
+            )
+            CommandUtils.chain(
+                [
+                    F, // /F
+                    cmd.literal('enchant_remove'),
+                    cmd.argument('type', arg.STRING.create(e)),
+                ],
+                [
+                    ctx => {
+                        let pool = getEnchantPool(CommandUtils.getPlayerItem(ctx))
                         if (!pool) return 0
                         Utils.server.tell(`Removing ${pool.length} enchantments`)
                         pool.clear()
                         return 1
-                    })
-                    .then(
-                        cmd.argument('type', arg.STRING.create(e)).executes(ctx => {
-                            let item = GetPlayerItem(ctx)
-                            let tag = item.getOrCreateTag()
-                            let key = getEnchantPool(item, true)
-                            let pool = tag[key]
-                            if (!pool) return 0
-                            let type = arg.STRING.getResult(ctx, 'type')
-                            Utils.server.tell(`Removing ${type} enchantments`)
-                            pool = pool.filter(x => x.id != type && x.id != `minecraft:${type}`)
-                            tag[key] = pool
-                            return 1
-                        }),
-                    ),
+                    },
+                    ctx => {
+                        let item = CommandUtils.getPlayerItem(ctx)
+                        let tag = item.getOrCreateTag()
+                        let key = getEnchantPool(item, true)
+                        let pool = tag[key]
+                        if (!pool) return 0
+                        let type = arg.STRING.getResult(ctx, 'type')
+                        Utils.server.tell(`Removing ${type} enchantments`)
+                        pool = pool.filter(x => x.id != type && x.id != `minecraft:${type}`)
+                        tag[key] = pool
+                        return 1
+                    },
+                ],
+                2,
             )
-            F.then(
-                cmd.literal('enchant_dump').executes(ctx => {
-                    let player = GetPlayer(ctx)
+            CommandUtils.chain(
+                [
+                    F, // /F
+                    cmd.literal('enchant_dump'),
+                ],
+                ctx => {
+                    let player = CommandUtils.getPlayer(ctx)
                     if (!player) return 0
                     let pool = getEnchantPool(player.getMainHandItem())
                     if (!pool) return 0
@@ -87,14 +81,19 @@ ServerEvents.commandRegistry(e => {
                     book.getOrCreateTag().merge({ StoredEnchantments: pool })
                     player.give(book)
                     return 1
-                }),
-            ).then(
-                cmd.literal('enchant_merge').executes(ctx => {
-                    let player = GetPlayer(ctx)
+                },
+            )
+            CommandUtils.chain(
+                [
+                    F, // /F
+                    cmd.literal('enchant_merge'),
+                ],
+                ctx => {
+                    let player = CommandUtils.getPlayer(ctx)
                     if (!player) return 0
                     let pool = getEnchantPool(player.getMainHandItem()),
                         poolOffhand = getEnchantPool(player.getOffhandItem())
-                    if (!poolOffhand) return Utils.server.tell('No enchantments in offhand'), 0
+                    if (!poolOffhand) return (Utils.server.tell('No enchantments in offhand'), 0)
                     if (!pool) {
                         let item = player.getMainHandItem()
                         let key = item.id == 'minecraft:enchanted_book' ? 'StoredEnchantments' : 'Enchantments'
@@ -118,38 +117,45 @@ ServerEvents.commandRegistry(e => {
                         mapType[id].lvl = Math.max(mapType[id].lvl, lvl)
                     }
                     return 1
-                }),
+                },
             )
         }
 
         // 重命名
         {
-            F.then(
-                cmd.literal('rename').then(
-                    cmd.argument('name', arg.STRING.create(e)).executes(ctx => {
-                        let item = GetPlayerItem(ctx)
-                        if (!item || item.id == 'minecraft:air') return 0
-                        let newName = arg.STRING.getResult(ctx, 'name')
-                        newName = newName.replace('\\', '\\\\').replace('"', '\\"') // 这是Java String，不是js string
-                        item.orCreateTag.display = { Name: `{"text":"${newName}"}` }
-                        return 1
-                    }),
-                ),
+            CommandUtils.chain(
+                [
+                    F, // /F
+                    cmd.literal('rename'),
+                    cmd.argument('name', arg.GREEDY_STRING.create(e)),
+                ],
+                ctx => {
+                    let item = CommandUtils.getPlayerItem(ctx)
+                    if (!item || item.id == 'minecraft:air') return 0
+                    let newName = arg.GREEDY_STRING.getResult(ctx, 'name')
+                    newName = newName.replace('\\', '\\\\').replace('"', '\\"') // 这是Java String，不是js string
+                    item.orCreateTag.display = { Name: `{"text":"${newName}"}` }
+                    return 1
+                },
             )
         }
 
         // 袭击冷却
         {
-            F.then(
-                cmd.literal('raidCD').executes(ctx => {
-                    let player = GetPlayer(ctx)
+            CommandUtils.chain(
+                [
+                    F, // /F
+                    cmd.literal('raidCD'),
+                ],
+                ctx => {
+                    let player = CommandUtils.getPlayer(ctx)
                     if (!player) return 0
                     let raid = player.level.getRaidAt(BlockPos(player.x, player.y, player.z))
-                    if (!raid) return
+                    if (!raid) return 0
                     global.setField(raid, 'f_37684_', Integer('1'))
                     player.level.raids.setDirty()
                     return 1
-                }),
+                },
             )
         }
 
@@ -160,30 +166,34 @@ ServerEvents.commandRegistry(e => {
 
             let GetOilData = ctx => {
                 let ret = {
-                    player: GetPlayer(ctx),
+                    player: CommandUtils.getPlayer(ctx),
                     save: OilChunksSavedData.load(ctx.source.level),
                 }
                 ret.cp = new ChunkPos(ret.player.blockPosition())
                 return ret
             }
 
-            F.then(
-                cmd
-                    .literal('cdg_oil_amount')
-                    .then(
-                        cmd.argument('amount', arg.INTEGER.create(e)).executes(ctx => {
-                            let { player, cp, save } = GetOilData(ctx)
-                            let newValue = arg.INTEGER.getResult(ctx, 'amount')
-                            save.setChunkAmount(cp, newValue)
-                            player.tell(`new amount: ${newValue}`)
-                            return 1
-                        }),
-                    )
-                    .executes(ctx => {
+            CommandUtils.chain(
+                [
+                    F, // /F
+                    cmd.literal('cdg_oil_amount'),
+                    cmd.argument('amount', arg.INTEGER.create(e)),
+                ],
+                [
+                    ctx => {
                         let { player, cp, save } = GetOilData(ctx)
                         player.tell(`amount: ${save.getChunkOilAmount(cp)}`)
                         return 1
-                    }),
+                    },
+                    ctx => {
+                        let { player, cp, save } = GetOilData(ctx)
+                        let newValue = arg.INTEGER.getResult(ctx, 'amount')
+                        save.setChunkAmount(cp, newValue)
+                        player.tell(`new amount: ${newValue}`)
+                        return 1
+                    },
+                ],
+                2,
             )
         }
 
---
+++ server_scripts/libs/CommandUtils.js
@@ -0,0 +1,36 @@
+let CommandUtils = {
+    /**
+     * @param {Internal.ArgumentBuilder<Internal.CommandSourceStack,any>[]} seq
+     * @param {((ctx:CommandSourceStack)=>number)|(((ctx:CommandSourceStack)=>number)[])} callback
+     * @param {number} callbackToLastN
+     */
+    chain(seq, callback, callbackToLastN) {
+        if (callback) {
+            if (callbackToLastN === undefined) callbackToLastN = 1
+            for (let i = 0; i < callbackToLastN; i++) {
+                let target = seq[seq.length - 1 - i]
+                let exec = callback.length && callback.forEach ? callback[callback.length - 1 - i] : callback
+                target.executes(exec)
+            }
+        }
+        for (let i = seq.length - 1; i > 0; i--) {
+            seq[i - 1] = seq[i - 1].then(seq[i])
+        }
+        return seq[0]
+    },
+    tryGetArg(ctx, argType, name) {
+        try {
+            return argType.getResult(ctx, name)
+        } catch (e) {}
+    },
+    getPlayer(ctx) {
+        /**@type {Player}*/
+        let player = ctx.source.entity
+        if (player && player.isPlayer()) return player
+    },
+    getPlayerItem(ctx) {
+        /**@type {Player}*/
+        let player = CommandUtils.getPlayer(ctx)
+        if (player) return player.getMainHandItem()
+    },
+}
