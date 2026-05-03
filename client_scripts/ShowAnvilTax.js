fc798b8112479ffa0c3eb5be386a06f5dd3ac61f
YukkuriC <799433638@qq.com>
2026-05-03 21:11:51 +0800

    repair tax display fix, `client` cleared

--- client_scripts/ShowAnvilTax.js
+++ client_scripts/ShowAnvilTax.js
@@ -1,6 +1,4 @@
-ItemEvents.tooltip(reg => {
-    reg.addAdvancedToAll((stack, _, tooltip) => {
-        let tax = stack.nbt?.RepairCost
-        if (tax) tooltip.add(Text.red(`Repair Cost: ${tax}`))
-    })
+NativeEvents.onEvent('net.neoforged.neoforge.event.entity.player.ItemTooltipEvent', e => {
+    let tax = e.itemStack.get('repair_cost')
+    if (tax) e.tooltip.add(Text.red(`Repair Cost: ${tax}`))
 })
